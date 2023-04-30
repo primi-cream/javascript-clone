1931d10f4da7688583cdafdd53566818d10d7ce9
PrimiCream <Drclockd@gmail.com>
2023-04-30 19:23:11 +0900

    ver 1.0 done

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "b005b077ea05be1c1ddea2dbebe6ef50";
 function onGioOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in ", lat, lon);
+    // console.log("You live in ", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
         const weather = document.querySelector("#weather span:first-child");
