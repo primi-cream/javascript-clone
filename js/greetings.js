e73388ad887c0647abdf6e37065c5930ac7b7e3d
PrimiCream <Drclockd@gmail.com>
2023-04-30 10:42:47 +0900

    paint Greetings

--- js/greetings.js
+++ js/greetings.js
@@ -1,15 +1,27 @@
 const greetings = document.querySelector("#greetings");
-const login = document.querySelector("#login > input");
+const login = document.querySelector("#login");
+const loginValue = document.querySelector("#login > input");
 
-const USERNAME_KEY = "";
+const USERNAME_KEY = "username";
 const HIDEN_CLASSNAME = "hidden";
 
 function getGreeting(event){
     if(event.key==="Enter"){
-        console.log(login.value);
-        const username = login.value;
+        console.log(loginValue.value);
+        const username = loginValue.value;
+        login.classList.add(HIDEN_CLASSNAME);
         localStorage.setItem(USERNAME_KEY, username);
+
+        paintGreetings(username);
     }
 }
 
+function paintGreetings(username){
+    greetings.innerText = `Hello ${username}`;
+}
+
+
 login.addEventListener("keyup", getGreeting)
+
+const saveUsername = localStorage.getItem(USERNAME_KEY);
+
