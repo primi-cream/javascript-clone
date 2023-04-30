5bf6a56afbb27e21490a85de5f85625edf4503b1
PrimiCream <Drclockd@gmail.com>
2023-04-30 10:11:26 +0900

    add localStorage

--- js/greetings.js
+++ js/greetings.js
@@ -1,9 +1,14 @@
 const greetings = document.querySelector("#greetings");
 const login = document.querySelector("#login > input");
 
+const USERNAME_KEY = "";
+const HIDEN_CLASSNAME = "hidden";
+
 function getGreeting(event){
     if(event.key==="Enter"){
         console.log(login.value);
+        const username = login.value;
+        localStorage.setItem(USERNAME_KEY, username);
     }
 }
 
